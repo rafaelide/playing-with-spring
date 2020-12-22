42f9e2c00e289853c0549b7cb3a09b0eea59d0f2
R.Ide <6502263+rafaelide@users.noreply.github.com>
2020-12-22 11:40:07 -0300

    add property injection

--- bean-lifecycle/src/main/java/com/rafaelide/spring/bean/lifecycle/Computer.java
+++ bean-lifecycle/src/main/java/com/rafaelide/spring/bean/lifecycle/Computer.java
@@ -3,6 +3,7 @@ package com.rafaelide.spring.bean.lifecycle;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -12,6 +13,12 @@ import javax.annotation.PreDestroy;
 public class Computer implements InitializingBean, DisposableBean, BeanNameAware {
 
     private CPU cpu;
+
+
+    private String serialNumber;
+
+    @Value("${computer.model}")
+    private String model;
 
     public Computer(CPU cpu) {
         System.out.println("Computer - executing constructor");
@@ -41,5 +48,31 @@ public class Computer implements InitializingBean, DisposableBean, BeanNameAware
     @Override
     public void setBeanName(String s) {
         System.out.println("Computer - executing implementation of BeanNameAware interface, method setBeanName()");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    @Value("${computer.serial-number}")
+    public void setSerialNumber(String serialNumber) {
+        System.out.println("Computer - injecting property through setter method");
+        this.serialNumber = serialNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "serialNumber='" + serialNumber + '\'' +
+                ", model='" + model + '\'' +
+                '}';
     }
 }
